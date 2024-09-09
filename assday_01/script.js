446636585a173de563aeef799eedf1da8239ef2a
“[firstname <ajayjadhav0913@gmail.com>
2024-09-10 02:21:02 +0530

    new keyword, classes, extends.... in js

--- assday_01/script.js
+++ assday_01/script.js
@@ -1,7 +1,38 @@
 
 
-function sum(a,b)
-{
-    return a+b;
+// function sum(a,b)
+// {
+//     return a+b;
+// }
+// console.log( sum(2,3));
+
+class user {
+   constructor(username)
+   {
+    this.username = username
+   }
+   logMe()
+   {
+    console.log(`username is : ${this.username}`);
+   }
 }
-console.log( sum(2,3));
+
+class teacher extends user{
+    constructor(username , emailid,password)
+    {   
+            super(username);
+        this.username = username;
+        this.emailid = emailid;
+        this.password = password;
+    }
+
+    aadcourse()
+    {
+        console.log(`this is the paid course ${this.username}`);
+    }
+}
+
+const chai = new teacher("ajay","ajay@123",'123');
+// console.log(chai);
+
+chai.aadcourse()
